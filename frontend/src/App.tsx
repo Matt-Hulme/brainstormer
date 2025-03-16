f765b129110c911eab2053940b1dcc50ccfa6782
matt-hulme <mattwhulme@gmail.com>
2025-03-16 17:29:50 -0600

    FE Started, boilerplate removed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,10 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-100">
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<div>Welcome to Brainstormer</div>} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-gray-100">
+      <main className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold">Brainstormer</h1>
+      </main>
+    </div>
   )
 }
 
