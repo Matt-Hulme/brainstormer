c48747741c6f131803f226625b524e5193487837
matt-hulme <mattwhulme@gmail.com>
2025-05-03 13:11:45 -0600

    home header started

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,10 @@
 export const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <header className="flex flex-row items-center justify-between p-[30px] w-full">
+        <span className="color-secondary-3 text-h4">AI tools for the creative minded</span>
+        <div>Log in</div>
+      </header>
+    </div>
+  )
 }
