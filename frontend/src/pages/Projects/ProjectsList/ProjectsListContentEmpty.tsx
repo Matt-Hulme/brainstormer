882c27958f53b1e62beb916b7a4bb2f52e6fc293
Matt Hulme <Mattwhulme@gmail.com>
2025-06-09 17:22:31 -0600

    sign-in modal copy change; projectslistcontent empty icon spacing change

--- frontend/src/pages/Projects/ProjectsList/ProjectsListContentEmpty.tsx
+++ frontend/src/pages/Projects/ProjectsList/ProjectsListContentEmpty.tsx
@@ -2,7 +2,7 @@ import emptyShapesImage from '../../../assets/home-empty-shapes.png'
 
 export const ProjectsListContentEmpty = () => {
   return (
-    <div className="flex flex-col justify-center pt-[90px] pb-[56px] gap-y-[50px]">
+    <div className="flex flex-col justify-center pt-[90px] gap-y-[50px]">
       <h1 className="color-secondary-4 text-h1">Looks like you don't have any projects (yet).</h1>
       <p className="pl-[124px] color-secondary-3 text-h4">
         Use the search bar above to start a new project.
@@ -10,7 +10,7 @@ export const ProjectsListContentEmpty = () => {
       <img
         src={emptyShapesImage}
         alt="Empty projects"
-        className="absolute bottom-0 pl-[124px] max-h-[460px] w-auto object-contain opacity-75"
+        className="pl-[124px] max-h-[460px] w-auto object-contain opacity-75"
       />
     </div>
   )
